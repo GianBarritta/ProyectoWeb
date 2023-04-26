dd8654e8b85ad92c86b670c77362f8c16075db56
Gian <gianbdeveloper@hotmail.com>
2023-04-25 21:26:21 -0300

    Render views

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render  
+from django.shortcuts import render, HttpResponse
 
+def  home(request):
 
+    return render(request,"ProyectoWebApp/Home.html")
 
+def  servicios(request):
 
+    return render(request,"ProyectoWebApp/Servicios.html")
+
+def  tienda(request):
+
+    return render(request,"ProyectoWebApp/Tienda.html")
+
+def  blog(request):
+
+    return render(request,"ProyectoWebApp/Blog.html")
+
+def  contacto(request):
+
+    return render(request,"ProyectoWebApp/Contacto.html")
