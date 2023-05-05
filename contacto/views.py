53adbe92794507f1eca77263d9cac88e7a06c863
Gian <gianbdeveloper@hotmail.com>
2023-05-05 16:52:00 -0300

    Contact form

--- contacto/views.py
+++ contacto/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .forms import FormularioContacto
 
 def  contacto(request):
 
-    return render(request,"contacto/contacto.html")
+    formulario_contacto = FormularioContacto()
+
+    return render(request,"contacto/contacto.html", {'miFormulario':formulario_contacto})
