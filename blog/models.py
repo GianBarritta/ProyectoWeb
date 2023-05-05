c12ee13dddca0a8bd8ee8dd01fba6eba2ea8c534
Gian <gianbdeveloper@hotmail.com>
2023-05-05 17:04:04 -0300

    Correcting nomenclature

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 class Categoria(models.Model):
 
-    nombre = models.CharField(max_length = 50)
-    created = models.DateTimeField(auto_now_add = True)
-    updated = models.DateTimeField(auto_now_add = True)
+    nombre = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name = 'categoria'
@@ -16,13 +16,13 @@ class Categoria(models.Model):
     
 class Post(models.Model):
 
-    titulo = models.CharField(max_length = 50)
-    contenido = models.CharField(max_length = 50)
-    imagen = models.ImageField(upload_to = 'blog', null=True, blank=True)
+    titulo = models.CharField(max_length=50)
+    contenido = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='blog', null=True, blank=True)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     categorias=models.ManyToManyField(Categoria)
-    created = models.DateTimeField(auto_now_add = True)
-    updated = models.DateTimeField(auto_now_add = True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name = 'post'
