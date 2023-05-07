fde16c8609c62e3364de3eacfa41bf5802a4f8ba
Gian <gianbdeveloper@hotmail.com>
2023-05-07 16:18:00 -0300

    Settings email II

--- contacto/views.py
+++ contacto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import FormularioContacto
+from django.core.mail import EmailMessage
 
 def  contacto(request):
 
@@ -11,7 +12,16 @@ def  contacto(request):
             nombre = request.POST.get("nombre")
             email = request.POST.get("email")
             contenido = request.POST.get("contenido")
+
+            email = EmailMessage("Mensaje desde App Django", 
+                                 "El usuario con nombre {} con la dirección {} escribe lo siguiente:\n\n {}".format(nombre,email,contenido),
+                                 "",["gianbarritta@gmail.com"],reply_to=[email])
+
+            try:
+                email.send()
+
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?novalido")
     
-            return redirect("/contacto/?valido")
-            
     return render(request,"contacto/contacto.html", {'miFormulario':formulario_contacto})
